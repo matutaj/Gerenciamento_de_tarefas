ef506c23069e9381c5fa0fa6b04082c30779cf4b
matutaj <matutajorge2002@gmail.com>
2025-01-09 17:52:56 -0300

    add configurações do tailwinds

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Tasks from "./components/Tasks";
 function App() {
   return (
     <div>
-      <h1 className="text-red-500 text-3xl">Gerenciador de Tarefas</h1>
+      <h1 className="text-red-500">Gerenciador de Tarefas</h1>
       <AddTasks />
       <Tasks />
     </div>
