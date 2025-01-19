2cc5accdd4419194b268e71cbd75ed5bc13423a3
matutajorge2002@gmail.com <matuta@bitkabir.com>
2025-01-19 06:26:18 -0300

    feat:  listar as tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
+import { useState } from "react";
 import AddTasks from "./components/AddTasks";
 import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar React",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar Vue",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Estudar Angular",
+      isCompleted: false,
+    },
+  ]);
   return (
-    <div>
-      <h1 className="text-red-500">Gerenciador de Tarefas</h1>
-      <AddTasks />
-      <Tasks />
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <AddTasks />
+        <Tasks tasks={tasks} />
+      </div>
     </div>
   );
 }
