0bc7ab81bee964285bdafbb1cc286916d5ffe7e7
matutajorge2002@gmail.com <matutajorge2002@gmail.com>
2025-01-20 12:09:56 -0300

    add

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,13 +1,12 @@
-
 import { ChevronRightIcon, TrashIcon } from "lucide-react";
 
-function Tasks(props) {
+function Tasks({ tasks, onTasksClick, onDeleteTasksClick }) {
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
-      {props.tasks.map((task) => (
+      {tasks.map((task) => (
         <li key={task.id} className="flex gap-2 ">
           <button
-            onClick={() => props.onTasksClick(task.id)}
+            onClick={() => onTasksClick(task.id)}
             className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${
               task.isCompleted && "line-through"
             }`}
@@ -17,10 +16,15 @@ function Tasks(props) {
           <button className="bg-slate-400 p-2 rounded-md text-white">
             <ChevronRightIcon />
           </button>
+          <button
+            onClick={() => onDeleteTasksClick(task.id)}
+            className="bg-slate-400 p-2 rounded-md text-white"
+          >
+            <TrashIcon />
+          </button>
         </li>
       ))}
     </ul>
   );
-
 }
 export default Tasks;
