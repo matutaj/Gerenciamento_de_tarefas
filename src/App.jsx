05a094ffc8c9f06d5207986709ef29909f09a366
matutajorge2002@gmail.com <matutajorge2002@gmail.com>
2025-01-21 17:12:40 -0300

    add button left and save date in localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddTasks from "./components/AddTasks";
 import Tasks from "./components/Tasks";
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      title: "Estudar React",
-      description: "Estudar React para se tornar cada vez melhor",
-      isCompleted: false,
-    },
-    {
-      id: 2,
-      title: "Estudar Vue",
-      description: "Estudar Vue para se tornar cada vez melhor",
-      isCompleted: false,
-    },
-    {
-      id: 3,
-      title: "Estudar Angular",
-      description: "Estudar Angular para se tornar cada vez melhor",
-      isCompleted: false,
-    },
-  ]);
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
 
   function onTasksClick(taskIs) {
     const newTasks = tasks.map((task) => {
@@ -48,6 +31,10 @@ function App() {
     };
     setTasks([...tasks, newsTasks]);
   }
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
--- src/pages/TasksPage.jsx
+++ src/pages/TasksPage.jsx
@@ -1,16 +1,25 @@
-import { useSearchParams } from "react-router-dom";
+import { ChevronLeftIcon } from "lucide-react";
+import { useSearchParams, useNavigate } from "react-router-dom";
 
 function TasksPage() {
+  const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const title = searchParams.get("title");
   const description = searchParams.get("description");
   return (
-    <div className=" h-screen w-screen bg-slate-500 p-6">
+    <div className=" h-screen w-screen bg-slate-500 flex justify-center  p-6">
       <div className="w-[500px] space-y-4">
-        <h1 className="text-3xl text-slate-100 font-bold text-center">
-          {" "}
-          Detalhes da Tarefa
-        </h1>
+        <div className="flex relative  justify-center mb-6 text-slate-100">
+          <button
+            onClick={() => navigate(-1)}
+            className="absolute left-0 top-0 bottom-0"
+          >
+            <ChevronLeftIcon />
+          </button>
+          <h1 className="text-3xl text-slate-100 font-bold text-center">
+            Detalhes da Tarefa
+          </h1>
+        </div>
         <div className="bg-slate-200 p-4 rounded-sm">
           <h1 className="text-xl  font-bold text-slate-600">{title}</h1>
           <p className="text-slate-600">{description}</p>
